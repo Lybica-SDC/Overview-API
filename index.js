5aa65777281e9636e2c9e6beb6a0e5312b3278ba
Johnathan Simeroth <john.simeroth@gmail.com>
2023-04-13 19:01:19 -0700

    added morgan

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const router = require('./server/routes');
 
 const app = express();
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../Lybica-Client/dist')));
